bb12016041781c3400fff560501ff076ee45c35b
soks29 <s.jesus@karizma.ma>
2023-01-16 16:02:21 +0100

    [FIX] correction du module kzm_instance_creation_portal et [ADD] module hr_payroll_13 pour la migration v13->v16

--- kzm_instance_creation_portal/controllers/controllers.py
+++ kzm_instance_creation_portal/controllers/controllers.py
@@ -1,34 +1,53 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+
+from odoo import http, _
 from odoo.http import request
-from odoo.addons.portal.controllers import portal
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+
+class InstanceCustomerPortal(CustomerPortal):
+
+    def _prepare_home_portal_values(self, counters):
+        values = super(InstanceCustomerPortal, self)._prepare_home_portal_values(counters)
+        create_id = http.request.env.context.get('uid')
+        instances_count = request.env['kzm.instance.request'].sudo().search_count([('create_uid', '=', create_id)])
+        values.update({'instances_count': instances_count, })
+        return values
 
 
 class KzmInstanceCreationPortal(http.Controller):
-    @http.route('/create_webform', type="http", auth="user", website=True)
-    def index(self, **kw):
-        return http.request.render('kzm_instance_creation_portal.create_instance', {
+    def _get_portal_default_domain(self):
+        my_user = request.env.user
+        return [
+            ('create_uid', '=', my_user.id),
+        ]
 
-        })
+    @http.route('/list', auth='public', website=True)
+    def index(self, sortby=None, filterby=None, **kw):
+        domain = self._get_portal_default_domain()
+        searchbar_sortings = {
+            'partner': {'label': _('Customer'), 'order': 'partner_id'},
+            'name': {'label': _('Name'), 'order': 'name'},
+            'odoo_version': {'label': _('Odoo version'), 'order': 'odoo_id'},
+        }
 
-    @http.route('/create/instance', type="http", auth="user", website=True)
-    def create_web_instance(self, **kw):
-        print("Data receved..........................", kw)
+        if not sortby:
+            sortby = 'partner'
+        order = searchbar_sortings[sortby]['order']
+
+        instances = request.env['kzm.instance.request'].sudo().search(domain, order=order)
+        return request.render('kzm_instance_creation_portal.list_instance_portal',
+                              {'instances': instances, 'page_name': 'instance',
+                               'searchbar_sortings': searchbar_sortings,
+                               'sortby': sortby,
+                               })
+
+    @http.route('/form_create_instance', auth='public', website=True)
+    def instance_form(self, **kw):
+        return request.render('kzm_instance_creation_portal.form_instance_portal', {})
+
+    @http.route('/create/instance', auth='public', website=True)
+    def instance_created(self, **kw):
+        kw['state'] = 'soumise'
         request.env['kzm.instance.request'].sudo().create(kw)
-        return request.render("kzm_instance_creation_portal.instance_thanks", {
-
-        })
-
-    @http.route(['/my/instance'], type='http', auth="user", website=True)
-    def counters(self, counters, **kw):
-        return self._prepare_home_portal_values(counters)
-
-
-class InstanceCustomerPortal(portal.CustomerPortal):
-    def _prepare_home_portal_values(self, counters):
-        values = super()._prepare_home_portal_values(counters)
-        count_instances = request.env['kzm.instance.request'].search_count([('state', 'in', ('brouillon', 'entraitement', 'traite'))])
-        values.update({
-            'count_instances': count_instances
-        })
-        return values
+        return request.render('kzm_instance_creation_portal.instance_created', {})
